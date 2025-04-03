822997e6925275751be9ead63007278819d6fbad
Pedro H. Chaves Maia <68562195+phchavesmaia@users.noreply.github.com>
2025-04-03 16:12:02 -0300

    first upload

---
+++ pycompress/__init__.py
@@ -0,0 +1,3 @@
+__version__ = "0.1.0"
+__author__ = 'Pedro H. Chaves Maia'
+__credits__ = 'Institute for Mobility and Social Development'
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='pycompress',
+    version='0.1.0',    
+    description='A python package to compress pandas DataFrames akin to Stata's `compress' command',
+    url='https://github.com/phchavesmaia/pycompress',
+    author='Pedro H. Chaves Maia',
+    author_email='pedro.maia@imdsbrasil.org',
+    license='MIT',
+    packages=['pycompress'],
+    install_requires=['pandas',
+                      'numpy',                     
+                      ],
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3.13',
+    ],
+)

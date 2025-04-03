cd47da7ff000ec648704eb8a7463f5ef7f17717e
PHCM <phchavesmaia@gmail.com>
2025-04-03 20:32:33 -0300

    setup made right

--- setup.py
+++ setup.py
@@ -8,15 +8,10 @@ setup(
     author='Pedro H. Chaves Maia',
     author_email='pedro.maia@imdsbrasil.org',
     license='MIT',
-<<<<<<< HEAD
     packages=['df_compress'],
-=======
-    packages=['df-compress'],
->>>>>>> cf9aba9bb7b86f77ce3b68f9b9d11ccbfa4bba7d
     install_requires=['pandas',
                       'numpy',                     
                       ],
-
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Science/Research',

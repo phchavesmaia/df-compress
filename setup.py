d89ff5c92d046a98c3c456f48bc5e493d63bba83
PHCM <phchavesmaia@gmail.com>
2025-04-03 21:56:02 -0300

    better setup

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup
+from pathlib import Path
+
+# Read the contents of README.md
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name='df-compress',
     version='0.2.0',    
     description="A python package to compress pandas DataFrames akin to Stata's `compress` command",
+    long_description=long_description,
+    long_description_content_type="text/markdown",  
     url='https://github.com/phchavesmaia/df-compress',
     author='Pedro H. Chaves Maia',
     author_email='pedro.maia@imdsbrasil.org',
